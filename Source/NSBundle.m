Distrotech/gnustep-base

commit b62c7680c82fe610d2c8709bb955257ba5b2c496

improve locating library bundles
git-svn-id: http://svn.gna.org/svn/gnustep/libs/base/trunk@27212 72102866-910b-0410-8b05-ffd578937521

Source/NSBundle.m
@@ -1179,10 +1179,6 @@ _bundle_load_callback(Class theClass, struct objc_category *theCategory)
 	   * Get the library bundle ... if there wasn't one then we
 	   * will assume the class was in the program executable and
 	   * return the mainBundle instead.
-	   *
-	   * FIXME: This will not work well with versioned library
-	   * resources.  It used to work fine (maybe) with unversioned
-	   * library resources.
 	   */
 	  bundle = [NSBundle bundleForLibrary: lib];
 	  if (bundle == nil)
@@ -2293,16 +2289,58 @@ _bundle_load_callback(Class theClass, struct objc_category *theCategory)
   NSEnumerator *enumerator;
   NSString *path;
   NSFileManager *fm = [NSFileManager defaultManager];
+  NSRange	r;
 
   /*
    * Eliminate any base path or extensions.
    */
   libraryName = [libraryName lastPathComponent];
-  do
+
+#if defined(__MINGW32__)
+  /* A dll is usually of the form 'xxx-maj_min.dll'
+   * so we can extract the version info and use it.
+   */
+  if ([[libraryName pathExtension] isEqual: @"dll"])
+    {
+      libraryName = [libraryName stringByDeletingPathExtension];
+      r = [libraryName rangeOfString: @"-" options: NSBackwardsSearch];
+      if (r.length > 0)
+	{
+	  NSString	*ver;
+
+	  ver = [[libraryName substringFromIndex: NSMaxRange(r)]
+	    stringByReplacingString: @"_" withString: @"."];
+	  libraryName = [libraryName substringToIndex: r.location];
+	  if (interfaceVersion == nil)
+	    {
+	      ver = interfaceVersion;
+	    }
+	}
+    }
+#else
+  /* A .so is usually of the form 'libxxx.so.maj.min.sub'
+   * so we can extract the version info and use it.
+   */
+  r = [libraryName rangeOfString: @".so."];
+  if (r.length > 0)
+    {
+      NSString	*s = [libraryName substringFromIndex: NSMaxRange(r)];
+      NSArray	*a = [s componentsSeparatedByString: @"."];
+
+      libraryName = [libraryName substringToIndex: r.location];
+      if (interfaceVersion == nil && [a count] >= 2)
+	{
+	  interfaceVersion = [NSString stringWithFormat: @"%@.%@",
+	    [a objectAtIndex: 0], [a objectAtIndex: 1]];
+	}
+    }
+#endif
+
+  while ([[libraryName pathExtension] length] > 0);
     {
       libraryName = [libraryName stringByDeletingPathExtension];
     }
-  while ([[libraryName pathExtension] length] > 0);
+
   /*
    * Discard leading 'lib'
    */

commit 174c006cc91b19061dd42ab8a4d74e48c6dfb43f

Fix infinite loop introduced in r27212.
git-svn-id: http://svn.gna.org/svn/gnustep/libs/base/trunk@27233 72102866-910b-0410-8b05-ffd578937521

Source/NSBundle.m
@@ -2350,7 +2350,7 @@ _bundle_load_callback(Class theClass, struct objc_category *theCategory)
     }
 #endif
 
-  while ([[libraryName pathExtension] length] > 0);
+  while ([[libraryName pathExtension] length] > 0)
     {
       libraryName = [libraryName stringByDeletingPathExtension];
     }